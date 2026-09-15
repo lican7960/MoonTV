5826234ded732646b8e8b0a35d22fa9030a39cc1
lican7960 <52392275+lican7960@users.noreply.github.com>
2026-09-15 08:43:14 +0800

    Update API endpoint for Bangumi calendar data

--- src/lib/bangumi.client.ts
+++ src/lib/bangumi.client.ts
@@ -23,7 +23,7 @@ export interface BangumiCalendarData {
 }
 
 export async function GetBangumiCalendarData(): Promise<BangumiCalendarData[]> {
-  const response = await fetch('https://api.bangumi.pro/calendar');
+  const response = await fetch('https://api.bangumi.vip/calendar');
   const data = await response.json();
   const filteredData = data.map((item: BangumiCalendarData) => ({
     ...item,
